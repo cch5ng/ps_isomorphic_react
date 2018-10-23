fa18b94a002b51461d6c2594f0e560d0381b0575
cch5ng <cch5ng@gmail.com>
2018-10-23 12:30:34 -0700

    add test for mapStateToProps

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,5 +1,35 @@
+import { mapStateToProps, QuestionDetailDisplay } from '../QuestionDetail'
 
 describe('question detail component', () => {
+  describe('Container element', () => {
+    describe('mapStateToProps', () => {
+
+      it('should map state to props correctly', () => {
+        const sampleQuestion = {
+          question_id: 42,
+          body: "blah blah blah"
+        }
+        const appState = {
+          questions: [sampleQuestion]
+        }
+        const ownProps = {
+          question_id: 42
+        }
+        const componentState = mapStateToProps(appState, ownProps)
+        expect(componentState).toEqual(sampleQuestion)
+        
+      })
+    })
+  })
+
+
+
+
+
+
+
+
+
 
   it('should not regress', () => {
 
