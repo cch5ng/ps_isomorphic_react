b1b1911cd205bc61187c26635eb844c23dd02380
cch5ng <cch5ng@gmail.com>
2018-10-22 17:01:45 -0700

    add snapshot test

---
+++ src/components/TagsList.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TagsList from './TagsList';
+import renderer from 'react-test-renderer';
+
+describe('TagsList component', () => {
+
+  it('should render', () => {
+    const tree = renderer
+      .create(<TagsList tags={[`css`, `html`, `go`]} />)
+      .toJSON();
+
+    console.log('tree', tree);
+
+    expect(tree).toMatchSnapshot();
+  })
+
+})
